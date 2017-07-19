6258f5025d448789de8e1e9182daebd80a3bed89
Thomas W <kontakt@themecoder.de>
2017-07-19 16:53:17 +0200

    added footer navigation

--- includes/modules/class-footer-line.php
+++ includes/modules/class-footer-line.php
@@ -33,7 +33,7 @@ class Chronus_Pro_Footer_Line {
 		add_action( 'chronus_footer_text', array( __CLASS__, 'footer_text' ) );
 
 		// Display footer navigation.
-		add_action( 'chronus_footer_menu', array( __CLASS__, 'display_footer_menu' ) );
+		add_action( 'chronus_before_footer', array( __CLASS__, 'display_footer_menu' ) );
 
 		// Add Footer Settings in Customizer.
 		add_action( 'customize_register', array( __CLASS__, 'footer_settings' ) );
