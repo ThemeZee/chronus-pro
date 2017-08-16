0ca9b0a8c2fed12c946a24b722ce2de9b157931c
Thomas W <kontakt@themecoder.de>
2017-08-16 11:43:24 +0200

    added Header Search feature

--- chronus-pro.php
+++ chronus-pro.php
@@ -114,6 +114,7 @@ class Chronus_Pro {
 		require_once CHRONUS_PRO_PLUGIN_DIR . 'includes/modules/class-footer-line.php';
 		require_once CHRONUS_PRO_PLUGIN_DIR . 'includes/modules/class-footer-widgets.php';
 		require_once CHRONUS_PRO_PLUGIN_DIR . 'includes/modules/class-header-bar.php';
+		require_once CHRONUS_PRO_PLUGIN_DIR . '/includes/modules/class-header-search.php';
 		require_once CHRONUS_PRO_PLUGIN_DIR . 'includes/modules/class-scroll-to-top.php';
 
 		// Include Magazine Widgets.
--- includes/customizer/class-customizer.php
+++ includes/customizer/class-customizer.php
@@ -59,6 +59,7 @@ class Chronus_Pro_Customizer {
 	static function get_default_options() {
 
 		$default_options = array(
+			'header_search'     => false,
 			'author_bio'        => false,
 			'scroll_to_top'     => false,
 			'footer_text'       => '',
