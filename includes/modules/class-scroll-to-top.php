9d92b55a35ea815248369f36493d06b9d81d2202
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-09-16 11:05:24 +0200

    disable scroll to top button when AMP is active

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -8,7 +8,9 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Scroll to Top Class
@@ -45,7 +47,7 @@ class Chronus_Pro_Scroll_To_Top {
 		$theme_options = Chronus_Pro_Customizer::get_theme_options();
 
 		// Call Credit Link function of theme if credit link is activated.
-		if ( true === $theme_options['scroll_to_top'] ) :
+		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
 			wp_enqueue_script( 'chronus-pro-scroll-to-top', CHRONUS_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.js', array( 'jquery' ), CHRONUS_PRO_VERSION, true );
 
@@ -65,8 +67,8 @@ class Chronus_Pro_Scroll_To_Top {
 		// Add Scroll to Top headline.
 		$wp_customize->add_control( new Chronus_Customize_Header_Control(
 			$wp_customize, 'chronus_theme_options[scroll_top_title]', array(
-				'label' => esc_html__( 'Scroll to Top', 'chronus-pro' ),
-				'section' => 'chronus_pro_section_footer',
+				'label'    => esc_html__( 'Scroll to Top', 'chronus-pro' ),
+				'section'  => 'chronus_pro_section_footer',
 				'settings' => array(),
 				'priority' => 10,
 			)
@@ -75,7 +77,7 @@ class Chronus_Pro_Scroll_To_Top {
 		// Add Scroll to Top setting.
 		$wp_customize->add_setting( 'chronus_theme_options[scroll_to_top]', array(
 			'default'           => false,
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'refresh',
 			'sanitize_callback' => 'chronus_sanitize_checkbox',
 		) );
@@ -87,6 +89,13 @@ class Chronus_Pro_Scroll_To_Top {
 			'type'     => 'checkbox',
 			'priority' => 20,
 		) );
+	}
+
+	/**
+	 * Checks if AMP page is rendered.
+	 */
+	static function is_amp() {
+		return function_exists( 'is_amp_endpoint' ) && is_amp_endpoint();
 	}
 }
 
