65083bbe88443256a50d7827c61583c0b7961616
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-09-26 10:48:27 +0200

    updated aria labels to improve accessibility

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Scroll to Top
+ * Scroll to top
  *
  * Displays scroll to top button based on theme options
  *
@@ -13,12 +13,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Scroll to Top Class
+ * Scroll to top Class
  */
 class Chronus_Pro_Scroll_To_Top {
 
 	/**
-	 * Scroll to Top Setup
+	 * Scroll to top Setup
 	 *
 	 * @return void
 	 */
@@ -49,10 +49,13 @@ class Chronus_Pro_Scroll_To_Top {
 		// Call Credit Link function of theme if credit link is activated.
 		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
-			wp_enqueue_script( 'chronus-pro-scroll-to-top', CHRONUS_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array(), '20220119', true );
+			wp_enqueue_script( 'chronus-pro-scroll-to-top', CHRONUS_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array(), '20220924', true );
 
 			// Passing Parameters to navigation.js.
-			wp_localize_script( 'chronus-pro-scroll-to-top', 'chronusProScrollToTop', array( 'icon' => chronus_get_svg( 'collapse' ) ) );
+			wp_localize_script( 'chronus-pro-scroll-to-top', 'chronusProScrollToTop', array(
+				'icon'  => chronus_get_svg( 'collapse' ),
+				'label' => esc_attr__( 'Scroll to top', 'chronus-pro' ),
+			) );
 
 		endif;
 	}
@@ -64,17 +67,17 @@ class Chronus_Pro_Scroll_To_Top {
 	 */
 	static function scroll_to_top_settings( $wp_customize ) {
 
-		// Add Scroll to Top headline.
+		// Add Scroll to top headline.
 		$wp_customize->add_control( new Chronus_Customize_Header_Control(
 			$wp_customize, 'chronus_theme_options[scroll_top_title]', array(
-				'label'    => esc_html__( 'Scroll to Top', 'chronus-pro' ),
+				'label'    => esc_html__( 'Scroll to top', 'chronus-pro' ),
 				'section'  => 'chronus_pro_section_footer',
 				'settings' => array(),
 				'priority' => 10,
 			)
 		) );
 
-		// Add Scroll to Top setting.
+		// Add Scroll to top setting.
 		$wp_customize->add_setting( 'chronus_theme_options[scroll_to_top]', array(
 			'default'           => false,
 			'type'              => 'option',
@@ -83,7 +86,7 @@ class Chronus_Pro_Scroll_To_Top {
 		) );
 
 		$wp_customize->add_control( 'chronus_theme_options[scroll_to_top]', array(
-			'label'    => __( 'Display Scroll to Top Button', 'chronus-pro' ),
+			'label'    => __( 'Display Scroll to top button', 'chronus-pro' ),
 			'section'  => 'chronus_pro_section_footer',
 			'settings' => 'chronus_theme_options[scroll_to_top]',
 			'type'     => 'checkbox',
